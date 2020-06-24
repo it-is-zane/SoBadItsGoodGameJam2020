4acf5b674f0300fa607c23d75331ff8581bcb78e
AbofHbou <zane.gant@gmail.com>
2020-06-24 01:32:20 -0700

    Organized Tools and split players control into a new node/scene.

--- Player/Player.gd
+++ Player/Player.gd
@@ -15,22 +15,27 @@ func _ready():
 	Animator.set("parameters/NotIdle/blend_amount",0)
 
 
+func right():
+	Animator.set("parameters/NotIdle/blend_amount",lerp(Animator.get("parameters/NotIdle/blend_amount"),1,0.1))
+	velocity.x = lerp(velocity.x,50,1)
+	get_node("0x72_DungeonTilesetII_v13").scale.x = 1
+
+func left():
+	Animator.set("parameters/NotIdle/blend_amount",lerp(Animator.get("parameters/NotIdle/blend_amount"),1,0.1))
+	velocity.x = lerp(velocity.x,-50,1)
+	get_node("0x72_DungeonTilesetII_v13").scale.x = -1
+
+func jump():
+	velocity.y = -60
+	Animator.set("parameters/Jump/active",true)
+
+func idle():
+	Animator.set("parameters/NotIdle/blend_amount",lerp(Animator.get("parameters/NotIdle/blend_amount"),0,0.1))
+	velocity.x = lerp(velocity.x,0,1)
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_pressed("ui_left"):
-		Animator.set("parameters/NotIdle/blend_amount",lerp(Animator.get("parameters/NotIdle/blend_amount"),1,0.1))
-		velocity.x = lerp(velocity.x,-50,1)
-		get_node("0x72_DungeonTilesetII_v13").scale.x = -1
-	if Input.is_action_pressed("ui_right"):
-		Animator.set("parameters/NotIdle/blend_amount",lerp(Animator.get("parameters/NotIdle/blend_amount"),1,0.1))
-		velocity.x = lerp(velocity.x,50,1)
-		get_node("0x72_DungeonTilesetII_v13").scale.x = 1
-	if Input.is_action_pressed("ui_up") and is_on_floor():
-		velocity.y = -60
-		Animator.set("parameters/Jump/active",true)
-	if not (Input.is_action_pressed("ui_right") or Input.is_action_pressed("ui_left")):
-		Animator.set("parameters/NotIdle/blend_amount",lerp(Animator.get("parameters/NotIdle/blend_amount"),0,0.1))
-		velocity.x = lerp(velocity.x,0,1)
 	velocity.y += 98 * delta
 	velocity = move_and_slide(velocity, Vector2.UP,
 					false, 4, PI/4, false)
